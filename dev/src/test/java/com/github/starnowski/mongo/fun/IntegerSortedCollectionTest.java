70e7dbaf9f044f78d9012e66d64d68e645b8545c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-08 00:08:45 +0100

    Added tests case.

--- dev/src/test/java/com/github/starnowski/mongo/fun/IntegerSortedCollectionTest.java
+++ dev/src/test/java/com/github/starnowski/mongo/fun/IntegerSortedCollectionTest.java
@@ -2,6 +2,7 @@ package com.github.starnowski.mongo.fun;
 
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.junit.jupiter.api.*;
@@ -27,24 +28,40 @@ import static java.util.Collections.singletonList;
 @ExtendWith(SpringExtension.class)
 public class IntegerSortedCollectionTest {
 
+    private static final String INTEGER_COLUMN = "integer_col";
+
     @Autowired
-    private  MongoTemplate mongoTemplate;
+    private MongoTemplate mongoTemplate;
     private MongoCollection<Document> sortable;
+
+    private static Stream<Arguments> provide_shouldReturnCorrectCountNumberAfterSeveralStagesWithDifferentCursorOperations() {
+        return Stream.of(
+                Arguments.of(asList(Aggregates.limit(600), Aggregates.skip(170), Aggregates.skip(200)), 230, "limit 600, skip 170, skip 200"),
+                Arguments.of(asList(Aggregates.skip(300), Aggregates.limit(400), Aggregates.skip(200)), 200, "skip 300, limit 400, skip 200")
+        );
+    }
+
+    private static Stream<Arguments> provide_shouldReturnCorrectFirstAndLastResultAfterSeveralStagesWithDifferentCursorOperations() {
+        return Stream.of(
+                Arguments.of(asList(Aggregates.limit(600), Aggregates.skip(170), Aggregates.skip(200)), 370, 599, "limit 600, skip 170, skip 200"),
+                Arguments.of(asList(Aggregates.limit(600), Aggregates.skip(170), Aggregates.skip(200), Aggregates.sort(Sorts.descending(INTEGER_COLUMN))), 599, 370, "limit 600, skip 170, skip 200, sort ascending"),
+                Arguments.of(asList(Aggregates.limit(600), Aggregates.skip(170), Aggregates.skip(200), Aggregates.sort(Sorts.descending(INTEGER_COLUMN)), Aggregates.skip(200)), 399, 370, "limit 600, skip 170, skip 200, sort ascending, skip 200")
+        );
+    }
 
     @BeforeAll
     public void setUp() {
         sortable = mongoTemplate.getCollection("integerSortedCollection");
         List<Document> documents = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
-            documents.add(new Document("i", i));
+            documents.add(new Document(INTEGER_COLUMN, i));
         }
         sortable.insertMany(documents);
     }
 
     @Test
     @DisplayName("should return 1000 as count result")
-    public void shouldReturnCorrectNumberForCountOperation()
-    {
+    public void shouldReturnCorrectNumberForCountOperation() {
         // GIVEN
         int expectedCount = 1000;
         Bson countBson = Aggregates.count();
@@ -60,8 +77,7 @@ public class IntegerSortedCollectionTest {
 
     @Test
     @DisplayName("should return 500 as count result after stage that limits result to 500 records")
-    public void shouldReturnCorrectNumberForCountOperationAfterLimit()
-    {
+    public void shouldReturnCorrectNumberForCountOperationAfterLimit() {
         // GIVEN
         int expectedCount = 500;
         Bson limitBson = Aggregates.limit(500);
@@ -81,8 +97,7 @@ public class IntegerSortedCollectionTest {
 
     @Test
     @DisplayName("should return 314 as count result after stage that skips 686 records")
-    public void shouldReturnCorrectCountNumberAfterSkipping()
-    {
+    public void shouldReturnCorrectCountNumberAfterSkipping() {
         // GIVEN
         int expectedCount = 314;
         Bson skipBson = Aggregates.skip(686);
@@ -102,8 +117,7 @@ public class IntegerSortedCollectionTest {
 
     @ParameterizedTest(name = "should return expected count number {1} after several stage operations : {2}")
     @MethodSource("provide_shouldReturnCorrectCountNumberAfterSeveralStagesWithDifferentCursorOperations")
-    public void shouldReturnCorrectCountNumberAfterSeveralStagesWithDifferentCursorOperations(List<Bson> stages, int expectedCount, String message)
-    {
+    public void shouldReturnCorrectCountNumberAfterSeveralStagesWithDifferentCursorOperations(List<Bson> stages, int expectedCount, String message) {
         // GIVEN
         List<Document> projection = new ArrayList<>();
         List<Bson> pipeline = new ArrayList<>();
@@ -118,11 +132,17 @@ public class IntegerSortedCollectionTest {
         Assertions.assertEquals(expectedCount, projection.get(0).getInteger("count"));
     }
 
-    private static Stream<Arguments> provide_shouldReturnCorrectCountNumberAfterSeveralStagesWithDifferentCursorOperations()
-    {
-        return Stream.of(
-                Arguments.of((List<Bson>)asList(Aggregates.limit(600), Aggregates.skip(170), Aggregates.skip(200)), 230, "limit 600, skip 170, skip 200"),
-                Arguments.of((List<Bson>)asList(Aggregates.skip(300), Aggregates.limit(400), Aggregates.skip(200)), 200, "skip 300, limit 400, skip 200")
-        );
+    @ParameterizedTest(name = "should return expected first number {1} and last number {2} after several stage operations : {3}")
+    @MethodSource("provide_shouldReturnCorrectFirstAndLastResultAfterSeveralStagesWithDifferentCursorOperations")
+    public void shouldReturnCorrectFirstAndLastResultAfterSeveralStagesWithDifferentCursorOperations(List<Bson> pipeline, int first, int last, String message) {
+        // GIVEN
+        List<Document> results = new ArrayList<>();
+
+        // WHEN
+        sortable.aggregate(pipeline).into(results);
+
+        // THEN
+        Assertions.assertEquals(first, results.get(0).getInteger(INTEGER_COLUMN));
+        Assertions.assertEquals(last, results.get(results.size() - 1).getInteger(INTEGER_COLUMN));
     }
 }
