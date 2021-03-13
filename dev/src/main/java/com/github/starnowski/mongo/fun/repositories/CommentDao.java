6e2d528ff81e88a651c87813c5568c12f9e15d78
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-13 18:34:20 +0100

    Code refactoring.

--- dev/src/main/java/com/github/starnowski/mongo/fun/repositories/CommentDao.java
+++ dev/src/main/java/com/github/starnowski/mongo/fun/repositories/CommentDao.java
@@ -1,7 +1,16 @@
 package com.github.starnowski.mongo.fun.repositories;
 
+import com.github.starnowski.mongo.fun.model.Comment;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CommentDao {
+    public Comment save(Comment comment) {
+        return null;
+    }
+
+    public Comment find(ObjectId oid) {
+        return null;
+    }
 }
--- dev/src/test/java/com/github/starnowski/mongo/fun/repositories/CommentDaoTest.java
+++ dev/src/test/java/com/github/starnowski/mongo/fun/repositories/CommentDaoTest.java
@@ -1,7 +1,36 @@
 package com.github.starnowski.mongo.fun.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.github.starnowski.mongo.fun.model.Comment;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class CommentDaoTest {
+
+    @Autowired
+    CommentDao commentDao;
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"comment1", "comment2", "commentFinal"})
+    public void shouldSavePost(String commentText) {
+        // GIVEN
+        Comment comment = new Comment();
+        String email = "john2001.doe@gmailcom";
+        comment.setEmail(email);
+        comment.setText(commentText);
+
+        // WHEN
+        Comment result = commentDao.save(comment);
+
+        // THEN
+        result = commentDao.find(result.getOid());
+        Assertions.assertNotNull(result.getId());
+        Assertions.assertEquals(commentText, result.getText());
+        Assertions.assertEquals(email, result.getEmail());
+    }
 
 }
