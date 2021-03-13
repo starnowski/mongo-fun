9875091a3203cc8f0144230519360d97524f52df
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-13 15:29:31 +0100

    Added tests cases for PostDao component.

--- dev/src/main/java/com/github/starnowski/mongo/fun/repositories/PostDao.java
+++ dev/src/main/java/com/github/starnowski/mongo/fun/repositories/PostDao.java
@@ -1,4 +1,9 @@
 package com.github.starnowski.mongo.fun.repositories;
 
+import com.github.starnowski.mongo.fun.model.Post;
+
 public class PostDao {
+    public Post save(Post post) {
+        return null;
+    }
 }
--- dev/src/test/java/com/github/starnowski/mongo/fun/repositories/PostDaoTest.java
+++ dev/src/test/java/com/github/starnowski/mongo/fun/repositories/PostDaoTest.java
@@ -1,7 +1,33 @@
 package com.github.starnowski.mongo.fun.repositories;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.github.starnowski.mongo.fun.model.Post;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class PostDaoTest {
 
+
+    @Autowired
+    PostDao postDao;
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"post1", "post2", "postFinal"})
+    public void shouldSavePost(String postText) {
+        // GIVEN
+        Post post = new Post();
+        String email = "john2001.doe@gmailcom";
+        post.setEmail(email);
+        post.setText(postText);
+
+        // WHEN
+        Post result = postDao.save(post);
+
+        // THEN
+        Assertions.assertNotNull(result.getId());
+        Assertions.assertEquals(postText, result.getText());
+        Assertions.assertEquals(email, result.getEmail());
+    }
 }
