68f5ebb92fb5a94db82efde3a871da62a2aa91ef
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-13 23:23:20 +0100

    Added implementation.

--- dev/src/main/java/com/github/starnowski/mongo/fun/services/PostService.java
+++ dev/src/main/java/com/github/starnowski/mongo/fun/services/PostService.java
@@ -1,16 +1,23 @@
 package com.github.starnowski.mongo.fun.services;
 
 import com.github.starnowski.mongo.fun.model.Post;
+import com.github.starnowski.mongo.fun.repositories.PostDao;
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
+
+    @Autowired
+    private PostDao postDao;
+
     public Post save(Post post) {
-        return null;
+        post = postDao.save(post);
+        return post;
     }
 
     public Post find(ObjectId oid) {
-        return null;
+        return postDao.find(oid);
     }
 }
