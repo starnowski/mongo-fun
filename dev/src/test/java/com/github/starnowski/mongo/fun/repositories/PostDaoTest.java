d332c08dc8450a0e65089fcced8d32d7af4423c4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-15 23:39:24 +0100

    Added tests cases for bulk writes with unordered order.

--- dev/src/test/java/com/github/starnowski/mongo/fun/repositories/PostDaoTest.java
+++ dev/src/test/java/com/github/starnowski/mongo/fun/repositories/PostDaoTest.java
@@ -81,6 +81,42 @@ class PostDaoTest {
         assertNull(postDao.find(postOid5));
     }
 
+    @Test
+    public void shouldSaveAllRecordsExceptThoseWhichFailedWhenUsingUnorderedBulkWrite() {
+        // GIVEN
+        // Create one post that will cause bulk write failure
+        Post post = postDao.save(new Post().withText("post XXX 1"));
+        ObjectId nonUniqueId = post.getOid();
+        ObjectId postOid1 = returnNonExistedObjectId();
+        ObjectId postOid2 = nonUniqueId;
+        ObjectId postOid3 = returnNonExistedObjectId();
+        ObjectId postOid4 = nonUniqueId;
+        ObjectId postOid5 = returnNonExistedObjectId();
+        List<WriteModel<Post>> bulkWrites = new ArrayList<>();
+        bulkWrites.add(insertOneModelForPostOid(postOid1));
+        bulkWrites.add(insertOneModelForPostOid(postOid2)); // Write should failed because non-unique key
+        bulkWrites.add(insertOneModelForPostOid(postOid3));
+        bulkWrites.add(insertOneModelForPostOid(postOid4)); // Write should failed because non-unique key
+        bulkWrites.add(insertOneModelForPostOid(postOid5));
+        BulkWriteOptions bulkWriteOptions = new BulkWriteOptions().ordered(false);
+
+        // WHEN
+        BulkWriteResult bulkWriteResult = null;
+        try {
+            postDao.getCollection().bulkWrite(bulkWrites, bulkWriteOptions);
+            Assertions.fail("expected an exception of type MongoBulkWriteException");
+        } catch (MongoBulkWriteException ex) {
+            bulkWriteResult = ex.getWriteResult();
+            bulkWriteResult.getInserts();
+        }
+
+        // THEN
+        assertEquals(3, bulkWriteResult.getInsertedCount());
+        assertNotNull(postDao.find(postOid1));
+        assertNotNull(postDao.find(postOid3));
+        assertNotNull(postDao.find(postOid5));
+    }
+
     private InsertOneModel<Post> insertOneModelForPostOid(ObjectId objectId) {
         return new InsertOneModel(new Post().withOid(objectId).withText("Test"));
     }
