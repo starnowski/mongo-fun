3bef5ea38a0b03066995c55221833518d5ba969a
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-20 00:25:33 +0100

    Added tests

---
+++ aggregation/test/setup.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const Lab = require('@hapi/lab')
+const Mongoose = require('mongoose')
+const { expect } = require('@hapi/code')
+
+const { describe, it } = (exports.lab = Lab.script())
+
+describe('MongoDB Single Instance ->', () => {
+  it('connects to MongoDB', async () => {
+    await expect(
+      Mongoose.connect('mongodb://localhost', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+    ).to.not.reject()
+  })
+
+  it('fails to connect to non-existent MongoDB instance', async () => {
+    await expect(
+      Mongoose.connect('mongodb://localhost:27018', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 1000,
+        serverSelectionTimeoutMS: 1000
+      })
+    ).to.reject()
+  })
+})
