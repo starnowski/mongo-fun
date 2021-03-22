934d671db5ef0d1d5ca55e8e19b3100bffd96b28
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-22 01:58:47 +0100

    Fixed test cases.

--- aggregation/test/jest/__tests__/mongo-first.js
+++ aggregation/test/jest/__tests__/mongo-first.js
@@ -30,6 +30,42 @@
 //  });
 //});
 
+//Install mongo on ubuntu : https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 //https://www.npmjs.com/package/mongodb
+//https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
 
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URI
+const uri =
+  "mongodb://localhost:27017/?readPreference=primary&ssl=false";
+// Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+//client.connect();
+//const db = client.db("admin").command({ ping: 1 });
+//const db = client.db("admin");
+
+
+describe("Basic mongo operations", () => {
+  test("should insert simple documents", async () => {
+
+    await client.connect();
+    // Establish and verify connection
+    const db = await client.db("admin");
+    const pizzaCollection = db.collection('pizzaCollection');
+    const options = { ordered: true };
+
+    const pizzaDocument = {
+      name: "Neapolitan pizza",
+      shape: "round",
+      toppings: [ "San Marzano tomatoes", "mozzarella di bufala cheese" ],
+    };
+    const result = await pizzaCollection.insertOne(pizzaDocument);
+    expect(result.insertedCount).toEqual(1);
+  });
+});
