9aca9a12c347d94302901f2b5d818648b71ae616
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2021-03-22 21:56:10 +0100

    Using the aggregation-tests database for tests purpose.

--- aggregation/test/jest/__tests__/mongo-first.js
+++ aggregation/test/jest/__tests__/mongo-first.js
@@ -1,46 +1,17 @@
-//https://jestjs.io/docs/mongodb
-
-
-//const {MongoClient} = require('mongodb');
-//
-//describe('insert', () => {
-//  let connection;
-//  let db;
-//
-//  beforeAll(async () => {
-//    connection = await MongoClient.connect(global.__MONGO_URI__, {
-//      useNewUrlParser: true,
-//    });
-//    db = await connection.db(global.__MONGO_DB_NAME__);
-//  });
-//
-//  afterAll(async () => {
-//    await connection.close();
-//    await db.close();
-//  });
-//
-//  test('should insert a doc into collection', async () => {
-//    const users = db.collection('users');
-//
-//    const mockUser = {_id: 'some-user-id', name: 'John'};
-//    await users.insertOne(mockUser);
-//
-//    const insertedUser = await users.findOne({_id: 'some-user-id'});
-//    expect(insertedUser).toEqual(mockUser);
-//  });
-//});
 
 //Install mongo on ubuntu : https://docs.mongodb.com/manual/tutorial/install-mongodb-on-ubuntu/
 //https://www.npmjs.com/package/mongodb
 //https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Async_await
 
+//How to create database in mongoDB
+//https://www.w3schools.com/nodejs/nodejs_mongodb_create_db.asp
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URI
 const uri =
-  "mongodb://localhost:27017/?readPreference=primary&ssl=false";
+  "mongodb://localhost:27017/aggregation-tests?readPreference=primary&ssl=false";
 // Create a new MongoClient
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -56,7 +27,7 @@ describe("Basic mongo operations", () => {
 
     await client.connect();
     // Establish and verify connection
-    const db = await client.db("admin");
+    const db = await client.db("aggregation-tests");
     const pizzaCollection = db.collection('pizzaCollection');
     const options = { ordered: true };
 
