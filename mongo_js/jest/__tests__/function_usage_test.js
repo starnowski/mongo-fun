409e0fe0f6aa4f996812700137bf7bb35b537e71
starnowski <33316705+starnowski@users.noreply.github.com>
2024-11-13 17:47:22 +0100

    Added tests case

--- mongo_js/jest/__tests__/function_usage_test.js
+++ mongo_js/jest/__tests__/function_usage_test.js
@@ -45,7 +45,7 @@ describe("Arrays mongo operations", () => {
     await arraysCollection.deleteMany(query);
   });
 
-    test("should return all documents where fronted developers are also backend developers", async () => {
+    test("should return all documents with initila for fronted developers", async () => {
      //GIVEN
      // Add data
      // this option prevents additional documents from being inserted if one fails
@@ -86,4 +86,48 @@ describe("Arrays mongo operations", () => {
       expect(result.every(elem => expectedTeams.includes(elem))).toBeTruthy();
       expect(result.length).toEqual(3);
     });
+
+    test("should update all documents with initila for fronted developers", async () => {
+      //GIVEN
+      // Add data
+      // this option prevents additional documents from being inserted if one fails
+      const options = { ordered: true };
+      const developersTeam = [
+               { t_id: "t1", developers : { fronted: ["Jake", "Mike"], backend: ["Mike", "Daniel", "Bill"] } },
+               { t_id: "t2", developers : { fronted: ["John"], backend: ["Mika", "Bill"] } },
+               { t_id: "t3", developers : { fronted: ["Simon", "Jake"], backend: ["Mika", "Jake", "Simon"] } },
+             ];
+      await arraysCollection.insertMany(developersTeam, options);
+      const expectedTeams = [JSON.stringify({t_id: "t1", initialsFD: "JM"}), JSON.stringify({t_id: "t2", initialsFD: "J"}),
+                 JSON.stringify({t_id: "t3", initialsFD: "SJ"})];
+ 
+       // WHEN
+       await arraysCollection.updateMany({}, 
+                                                      [{
+                                                         $set: {
+                                                             initialsFD: {
+                                                                 $function: {
+                                                                     body: getNamesInitials.toString(),
+                                                                     args: ["$developers.fronted"],
+                                                                     lang: "js"
+                                                                 }
+                                                             }
+                                                         }
+                                                     }
+                                                       ]);
+ 
+       // THEN
+       var result = await arraysCollection.aggregate([
+          {
+              $project: { t_id: 1, initialsFD: 1}
+          }
+            ]).toArray();
+       console.log('result: ' + result);
+       console.log(result);
+       result = result.map(function (doc) { return JSON.stringify({ t_id: doc.t_id, initialsFD: doc.initialsFD }) });
+       console.log('current teams: ' + result);
+       console.log('expected teams: ' + expectedTeams);
+       expect(result.every(elem => expectedTeams.includes(elem))).toBeTruthy();
+       expect(result.length).toEqual(3);
+     });
 });
