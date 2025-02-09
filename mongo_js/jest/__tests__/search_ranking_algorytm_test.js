bf19fab8d92992906ee7a47212ac32874ce54371
starnowski <33316705+starnowski@users.noreply.github.com>
2025-02-09 18:43:21 +0100

    Fixed setting data

--- mongo_js/jest/__tests__/search_ranking_algorytm_test.js
+++ mongo_js/jest/__tests__/search_ranking_algorytm_test.js
@@ -30,9 +30,38 @@ beforeAll( async () => {
     { prop1: 1 } );
   await arraysCollection.createIndex(
       { prop2: 1 } );
+
+  const query = { _id: {$exists: true} };
+  await arraysCollection.deleteMany(query);
+  const options = { ordered: true };
+  const developersTeam = [
+                { r_1: "t1", prop1: "A", prop2: 12 },
+                { r_1: "t2", prop1: "B", prop2: 13 },
+                { r_1: "t3", prop1: "C", prop2: 14 },
+                { r_1: "t4", prop1: "D", prop2: 15 },
+                { r_1: "t5", prop1: "E", prop2: 16 },
+                { r_1: "t6", prop1: "F", prop2: 17 },
+                { r_1: "t7", prop1: "G", prop2: 18 },
+                { r_1: "t8", prop1: "A", prop2: 19 },
+                { r_1: "t9", prop1: "S", prop2: 22 },
+                { r_1: "t10", prop1: "K", prop2: 231 },
+                { r_1: "t11", prop1: "A", prop2: 443 },
+                { r_1: "t12", prop1: "B", prop2: 765 },
+                { r_1: "t13", prop1: "C", prop2: 23 },
+                { r_1: "t14", prop1: "D", prop2: 87 },
+                { r_1: "t15", prop1: "F", prop2: 990 },
+                { r_1: "t16", prop1: "AD", prop2: 2 },
+                { r_1: "t17", prop1: "BC", prop2: 313 },
+                { r_1: "t18", prop1: "TU", prop2: 54 },
+                { r_1: "t19", prop1: "YA", prop2: 7 },
+                { r_1: "t20", prop1: "TAJ", prop2: 12 }
+              ];
+  await arraysCollection.insertMany(developersTeam, options);
 });
 
 afterAll(async () => {
+  const query = { _id: {$exists: true} };
+  await arraysCollection.deleteMany(query);
   // Close client
   await client.close();
 });
@@ -40,33 +69,6 @@ afterAll(async () => {
 describe("Arrays mongo operations", () => {
   beforeEach(async () => {
     console.log("Running tests on mongodb : " + mongoUrl);
-    arraysCollection = db.collection('arraysCollection');
-    const query = { _id: {$exists: true} };
-    await arraysCollection.deleteMany(query);
-    const options = { ordered: true };
-     const developersTeam = [
-              { r_1: "t1", prop1: "A", prop2: 12 },
-              { r_1: "t2", prop1: "B", prop2: 13 },
-              { r_1: "t3", prop1: "C", prop2: 14 },
-              { r_1: "t4", prop1: "D", prop2: 15 },
-              { r_1: "t5", prop1: "E", prop2: 16 },
-              { r_1: "t6", prop1: "F", prop2: 17 },
-              { r_1: "t7", prop1: "G", prop2: 18 },
-              { r_1: "t8", prop1: "A", prop2: 19 },
-              { r_1: "t9", prop1: "S", prop2: 22 },
-              { r_1: "t10", prop1: "K", prop2: 231 },
-              { r_1: "t11", prop1: "A", prop2: 443 },
-              { r_1: "t12", prop1: "B", prop2: 765 },
-              { r_1: "t13", prop1: "C", prop2: 23 },
-              { r_1: "t14", prop1: "D", prop2: 87 },
-              { r_1: "t15", prop1: "F", prop2: 990 },
-              { r_1: "t16", prop1: "AD", prop2: 2 },
-              { r_1: "t17", prop1: "BC", prop2: 313 },
-              { r_1: "t18", prop1: "TU", prop2: 54 },
-              { r_1: "t19", prop1: "YA", prop2: 7 },
-              { r_1: "t20", prop1: "TAJ", prop2: 12 }
-            ];
-     await arraysCollection.insertMany(developersTeam, options);
   });
 
     test("should return all documents with initila for fronted developers", async () => {
