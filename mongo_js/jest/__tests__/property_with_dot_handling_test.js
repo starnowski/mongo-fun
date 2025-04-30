fe2ee15e252ad8fbb1d2d11fc95bbfa81a8aaa97
starnowski <33316705+starnowski@users.noreply.github.com>
2025-04-30 20:41:27 +0200

    Added tests [skip ci]

--- mongo_js/jest/__tests__/property_with_dot_handling_test.js
+++ mongo_js/jest/__tests__/property_with_dot_handling_test.js
@@ -106,4 +106,93 @@ describe("Mongo operations on properties with dot character", () => {
       console.log('expected documents: ' + JSON.stringify(expectedRecords));
       expect(expectedRecords).toEqual(result);
      });
+
+    test("should update specific documents with field that has dot and also do query based on nested property", async () => {
+      //GIVEN
+      // Add data
+      // this option prevents additional documents from being inserted if one fails
+      const options = { ordered: true };
+      const developersTeam = [
+        { t_id: "t1", top: { "nested.prop1": { "prop1": 1, prop2: 2 }, "prop_level2": { prop3: 13} } },
+        { t_id: "t2", top: { "nested.prop1": { "prop1": 100, prop2: 276 }, "prop_level2": { prop3: 13} } }
+             ];
+      await arraysCollection.insertMany(developersTeam, options);
+      const expectedRecords = [
+        { t_id: "t1", top: { "nested.prop1": { "prop1": 47, prop2: 2 }, "prop_level2": { prop3: 13} } },
+        { t_id: "t2", top: { "nested.prop1": { "prop1": 100, prop2: 276 }, "prop_level2": { prop3: 13} } }
+      ];
+ 
+       // WHEN
+       await arraysCollection.aggregate(
+                                                      [
+                                                        {
+                                                          $match: {
+                                                            $eq: [
+                                                              {
+                                                                $getField: {
+                                                                  field: "prop1",
+                                                                  input: {
+                                                                    $getField: {
+                                                                      field: "nested.prop1",
+                                                                      input: "$$CURRENT.top"
+                                                                    }
+                                                                  }
+                                                                }
+                                                              },
+                                                              1
+                                                            ]
+                                                          }
+                                                        },
+                                                        {
+                                                         $set: {
+                                                             "tmpField": {
+                                                              $getField: {
+                                                                field: "nested.prop1",
+                                                                input: "$$CURRENT.top"
+                                                              }
+                                                             }
+                                                         }
+                                                      },
+                                                      {
+                                                        $set: {
+                                                            "tmpField.prop1": 47
+                                                            }
+                                                        }
+                                                        ,
+                                                        { $set : {
+                                                          "top": {
+                                                              $setField: {
+                                                                  field: "nested.prop1",
+                                                                  input: "$$CURRENT.top",
+                                                                  value: "$tmpField"
+                                                                  }
+                                                          }
+                                                        }
+                                                      },
+                                                        {
+                                                          $unset: "tmpField"
+                                                        }
+                                                        ,
+                                                        {
+                                                          $merge: {
+                                                            into: "arraysCollection",
+                                                            on: "_id",
+                                                            whenMatched: "replace",
+                                                            whenNotMatched: "fail"
+                                                          }
+                                                        }
+                                                       ]);
+ 
+       // THEN
+       var result = await arraysCollection.aggregate([
+          {
+              $project: { _id: 0}
+          }
+            ]).toArray();
+       console.log('result: ' + result);
+       console.log(JSON.stringify(result));
+      console.log('current documents: ' + JSON.stringify(result));
+      console.log('expected documents: ' + JSON.stringify(expectedRecords));
+      expect(expectedRecords).toEqual(result);
+     });
 });
