a8df3329ba99817abec9c2147b503d622798847b
starnowski <33316705+starnowski@users.noreply.github.com>
2025-09-02 10:13:19 +0200

    Added filters [skip ci]

--- mongodb-embedded-quarkus/README.md
+++ mongodb-embedded-quarkus/README.md
@@ -4,3 +4,9 @@
 https://github.com/quarkusio/quarkus/blob/main/docs/src/main/asciidoc/mongodb.adoc
 
 
+
+
+#### OpenApi schema validator
+
+https://github.com/networknt/json-schema-validator?utm_source=chatgpt.com
+
--- mongodb-embedded-quarkus/src/main/java/com/github/starnowski/mongo/fun/mongodb/container/filters/OpenApiJsonValidator.java
+++ mongodb-embedded-quarkus/src/main/java/com/github/starnowski/mongo/fun/mongodb/container/filters/OpenApiJsonValidator.java
@@ -1,5 +1,6 @@
 package com.github.starnowski.mongo.fun.mongodb.container.filters;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.schema.JsonSchema;
@@ -8,11 +9,33 @@ import com.networknt.schema.SpecVersion;
 import com.networknt.schema.ValidationMessage;
 import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.oas.models.OpenAPI;
+import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.Map;
 import java.util.Set;
 
+@ApplicationScoped
 public class OpenApiJsonValidator {
     private static final ObjectMapper mapper = new ObjectMapper();
+    private final OpenAPI openAPI;
+
+    public OpenApiJsonValidator() {
+        openAPI = new OpenAPIV3Parser().read("src/main/resources/example_openapi.yaml");
+    }
+
+    public Set<ValidationMessage> validateObject(String model, String json) throws JsonProcessingException {
+        String schemaString = mapper.writeValueAsString(
+                openAPI.getComponents().getSchemas().get(model)
+        );
+
+        // 3. Build schema validator
+        JsonSchemaFactory factory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
+        JsonSchema schema = factory.getSchema(schemaString);
+
+        // 4. Parse and validate input JSON
+        JsonNode jsonNode = mapper.readTree(json);
+        return schema.validate(jsonNode);
+    }
 
     public static void main(String[] args) throws Exception {
         String openApiPath = "src/main/resources/openapi.yaml"; // Load dynamically
