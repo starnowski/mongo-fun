e94e5a66dc67f9ce5bdae05b75ceb958902f9b09
starnowski <33316705+starnowski@users.noreply.github.com>
2025-09-19 00:08:59 +0200

    Added implementation for wrapped functions

--- mongodb-quarkus-odata/src/main/java/com/github/starnowski/mongo/fun/mongodb/container/repositories/ExampleDao.java
+++ mongodb-quarkus-odata/src/main/java/com/github/starnowski/mongo/fun/mongodb/container/repositories/ExampleDao.java
@@ -1,18 +1,15 @@
 package com.github.starnowski.mongo.fun.mongodb.container.repositories;
 
 import com.github.starnowski.mongo.fun.mongodb.container.odata.Example2StaticEdmSupplier;
-import com.github.starnowski.mongo.fun.mongodb.container.odata.MongoFilterVisitor;
 import com.github.starnowski.mongo.fun.mongodb.container.odata.ODataToMongoParser;
 import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.apache.olingo.commons.api.edm.Edm;
 import org.apache.olingo.server.api.OData;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.uri.UriInfo;
 import org.apache.olingo.server.api.uri.queryoption.FilterOption;
-import org.apache.olingo.server.api.uri.queryoption.expression.Expression;
 import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
 import org.apache.olingo.server.core.uri.parser.Parser;
 import org.apache.olingo.server.core.uri.parser.UriParserException;
@@ -21,13 +18,17 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-import static com.github.starnowski.mongo.fun.mongodb.container.odata.Example2StaticEdmProvider.ENTITY_SET_NAME;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 @ApplicationScoped
 public class ExampleDao extends AbstractDao<Document> {
+
+    @Inject
+    private Example2StaticEdmSupplier example2StaticEdmSupplier;
 
     @Override
     protected String getCollectionName() {
@@ -39,25 +40,34 @@ public class ExampleDao extends AbstractDao<Document> {
         return Document.class;
     }
 
-    @Inject
-    private Example2StaticEdmSupplier example2StaticEdmSupplier;
-
     public List<Document> query(String filter) throws ExpressionVisitException, ODataApplicationException, UriValidationException, UriParserException {
+        List<Bson> pipeline = preparePipelineBasedOnFilter(Collections.singletonList(filter));
+
+        return pipeline.isEmpty() ? new ArrayList<>() : getCollection().aggregate(pipeline).into(new ArrayList<>());
+    }
+
+    private List<Bson> preparePipelineBasedOnFilter(List<String> filters) throws UriValidationException, UriParserException, ExpressionVisitException, ODataApplicationException {
         List<Bson> pipeline = new ArrayList<>();
 
-        if (filter != null && !filter.isEmpty()) {
-                // Parse OData $filter into UriInfo (simplified)
-                UriInfo uriInfo = new Parser(example2StaticEdmSupplier.get(), OData.newInstance())
-                        .parseUri("examples2", "$filter=" + filter, null, null);
+        if (filters != null && !filters.isEmpty() &&
+                filters.stream().filter(Objects::nonNull).anyMatch(filter -> !filter.trim().isEmpty())) {
+            // Parse OData $filter into UriInfo (simplified)
+            UriInfo uriInfo = new Parser(example2StaticEdmSupplier.get(), OData.newInstance())
+                    .parseUri("examples2",
+//                            "$filter=" + filter
+                            filters.stream().filter(Objects::nonNull)
+                                    .filter(filter -> !filter.trim().isEmpty())
+                                    .map(filter -> "$filter=" + filter)
+                                    .collect(Collectors.joining("&"))
+                            , null, null);
 
-                FilterOption filterOption = uriInfo.getFilterOption();
-                if (filterOption != null) {
-                    Bson bsonFilter = ODataToMongoParser.parseFilter(uriInfo);
-                    pipeline.add(Aggregates.match(Filters.and(bsonFilter)));
-                }
+            FilterOption filterOption = uriInfo.getFilterOption();
+            if (filterOption != null) {
+                Bson bsonFilter = ODataToMongoParser.parseFilter(uriInfo);
+                pipeline.add(Aggregates.match(Filters.and(bsonFilter)));
+            }
 
         }
-
-        return getCollection().aggregate(pipeline).into(new ArrayList<>());
+        return pipeline;
     }
 }
