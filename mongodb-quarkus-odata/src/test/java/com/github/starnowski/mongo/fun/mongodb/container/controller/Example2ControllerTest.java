05f2dafdfb862c73bdd7e6e4a4234ccc984bc46b
starnowski <33316705+starnowski@users.noreply.github.com>
2025-09-21 20:59:38 +0200

    Added test case for date-time [skip ci]

--- mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerTest.java
+++ mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerTest.java
@@ -67,7 +67,8 @@ class Example2ControllerTest {
                 Arguments.of(List.of("plainString eq 'Some text'"), "examples/query/responses/example2_2.json"),
                 Arguments.of(List.of("startswith(plainString,'So')"), "examples/query/responses/example2_2.json"),
                 Arguments.of(Arrays.asList("startswith(plainString,'So')", "plainString eq 'Some text'"), "examples/query/responses/example2_2.json"),
-                Arguments.of(Arrays.asList("startswith(plainString,'Some t')", "smallInteger eq -1188957731"), "examples/query/responses/example2_2.json")
+                Arguments.of(Arrays.asList("startswith(plainString,'Some t')", "smallInteger eq -1188957731"), "examples/query/responses/example2_2.json"),
+                Arguments.of(Arrays.asList("timestamp ge 2024-07-20T10:00:00.00Z", "timestamp le 2024-07-20T20:00:00.00Z"), "examples/query/responses/example2_1.json")
         );
     }
 
