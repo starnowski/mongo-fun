6f997c90c8521838b2c71b9cca720703814d1f3b
starnowski <33316705+starnowski@users.noreply.github.com>
2025-09-29 12:15:31 +0200

    Added tests case for elementMatch

--- mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerTest.java
+++ mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerTest.java
@@ -115,8 +115,9 @@ class Example2ControllerTest {
                 Arguments.of(List.of("tags/any(t:t eq 'developer') or tags/any(t:t eq 'LLM')"), "examples/query/responses/example2_3.json", "COLLSCAN"),
                 Arguments.of(List.of("tags/any(t:startswith(t,'dev'))"), "examples/query/responses/example2_3.json", "COLLSCAN"),
                 Arguments.of(List.of("tags/any(t:startswith(t,'dev') and length(t) eq 9)"), "examples/query/responses/example2_3.json", "COLLSCAN"),
-                Arguments.of(List.of("tags/any(t:length(t) eq 9)"), "examples/query/responses/example2_3.json", "COLLSCAN"),
-                Arguments.of(List.of("tags/any(t:tolower(t) eq 'developer')"), "examples/query/responses/example2_3.json", "COLLSCAN")
+                Arguments.of(List.of("tags/any(t:length(t) eq 13)"), "examples/query/responses/example2_1.json", "COLLSCAN"),
+                Arguments.of(List.of("tags/any(t:tolower(t) eq 'developer')"), "examples/query/responses/example2_3.json", "COLLSCAN"),
+                Arguments.of(List.of("tags/any(t:startswith(t,'spider') and endswith(t, 'web')"), "examples/query/responses/example2_2.json", "COLLSCAN")
         );
     }
 
