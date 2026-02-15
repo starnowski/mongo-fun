cf174c8bdbb77d21250a44502817d5d01a3e5690
starnowski <33316705+starnowski@users.noreply.github.com>
2026-02-15 01:16:06 +0100

    #5 - Added tests cases [skip ci]

--- mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerAnyLambdaTest.java
+++ mongodb-quarkus-odata/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/controller/Example2ControllerAnyLambdaTest.java
@@ -77,7 +77,53 @@ class Example2ControllerAnyLambdaTest extends AbstractExample2ControllerTest {
             prepareResponseForQueryWithPlainStringProperties("Some text", "Poem", "Oleksa")),
         Arguments.of(
             List.of("numericArray/any(n:n add 2 gt 100)"),
-            prepareResponseForQueryWithPlainStringProperties("Mario")));
+            prepareResponseForQueryWithPlainStringProperties("Mario")),
+        Arguments.of(
+            List.of("tags/any(t:t ne 'no such text' and t ne 'no such word')"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star') and t ne 'starlord')"),
+            prepareResponseForQueryWithPlainStringProperties("Mario", "Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star') or t ne 'starlord')"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'star ') or t eq 'starlord')"),
+            prepareResponseForQueryWithPlainStringProperties("Mario", "Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:startswith(t,'starlord') or t in ('star trek', 'star wars'))"),
+            prepareResponseForQueryWithPlainStringProperties("Mario", "Oleksa")),
+        Arguments.of(
+            List.of(
+                "tags/any(t:contains(t,'starlord') or contains(t,'trek') or contains(t,'wars'))"),
+            prepareResponseForQueryWithPlainStringProperties("Mario", "Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:contains(t,'starlord'))"),
+            prepareResponseForQueryWithPlainStringProperties("Oleksa")),
+        Arguments.of(
+            List.of("tags/any(t:endswith(t,'web') or endswith(t,'trap'))"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text")),
+        Arguments.of(
+            List.of("tags/any(t:length(t) eq 9)"),
+            prepareResponseForQueryWithPlainStringProperties("Some text", "Poem", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("numericArray/any(n:n gt 5)"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("numericArray/any(n:n gt floor(5.05))"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("numericArray/any(n:n add 2 gt round(n))"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "eOMtThyhVNLWUZNRcBaQKxI", "Some text", "Poem", "Mario", "Oleksa")),
+        Arguments.of(
+            List.of("numericArray/any(n:n eq 10 or n eq 20 or n eq 30)"),
+            prepareResponseForQueryWithPlainStringProperties("eOMtThyhVNLWUZNRcBaQKxI")));
   }
 
   public static Stream<Arguments> provideShouldReturnResponseStringBasedOnComplexListFilters() {
@@ -93,7 +139,22 @@ class Example2ControllerAnyLambdaTest extends AbstractExample2ControllerTest {
             prepareResponseForQueryWithPlainStringProperties("Doc2", "Doc3")),
         Arguments.of(
             List.of("complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1'))"),
-            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc2", "Doc4")));
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc2", "Doc4")),
+        Arguments.of(
+            List.of("complexList/any(c:startswith(c/someString,'Ap'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc4", "Doc5")),
+        Arguments.of(
+            List.of("complexList/any(c:contains(c/someString,'ana'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc2")),
+        Arguments.of(
+            List.of("complexList/any(c:endswith(c/someString,'erry'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc3", "Doc4")),
+        Arguments.of(
+            List.of("complexList/any(c:contains(c/someString,'e'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc3", "Doc4", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someString eq 'Application')"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc5")));
   }
 
   @ParameterizedTest
@@ -152,6 +213,161 @@ class Example2ControllerAnyLambdaTest extends AbstractExample2ControllerTest {
             collection = "examples")
       })
   public void shouldReturnResponseStringBasedOnComplexListFilters(
+      List<String> filters, String expectedResponse) throws IOException, JSONException {
+    // WHEN
+    ExtractableResponse<Response> getResponse =
+        given()
+            .when()
+            .queryParams(Map.of("$filter", filters))
+            .get("/examples2/simple-query")
+            .then()
+            .statusCode(200)
+            .extract();
+
+    // THEN
+    JSONAssert.assertEquals(expectedResponse, getResponse.asString(), false);
+  }
+
+  public static Stream<Arguments>
+      provideShouldReturnResponseStringBasedOnComplexListFiltersWithNumericProperties() {
+    return Stream.of(
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber gt 5)"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber gt 25)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc2", "Doc3", "Doc4", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber lt 25)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc4", "Doc5")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber eq 10 or c/someNumber eq 20)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc4", "Doc5")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber add 5 gt 20)"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber gt floor(5.05))"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber add 2 gt round(c/someNumber))"),
+            prepareResponseForQueryWithPlainStringProperties(
+                "Doc1", "Doc2", "Doc3", "Doc4", "Doc5", "Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/someNumber eq 20)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc5")));
+  }
+
+  @ParameterizedTest
+  @MethodSource({"provideShouldReturnResponseStringBasedOnComplexListFiltersWithNumericProperties"})
+  @MongoSetup(
+      mongoDocuments = {
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_1.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_2.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_3.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_4.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_5.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_6.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_only_id.json",
+            collection = "examples")
+      })
+  public void shouldReturnResponseStringBasedOnComplexListFiltersWithNumericProperties(
+      List<String> filters, String expectedResponse) throws IOException, JSONException {
+    // WHEN
+    ExtractableResponse<Response> getResponse =
+        given()
+            .when()
+            .queryParams(Map.of("$filter", filters))
+            .get("/examples2/simple-query")
+            .then()
+            .statusCode(200)
+            .extract();
+
+    // THEN
+    JSONAssert.assertEquals(expectedResponse, getResponse.asString(), false);
+  }
+
+  public static Stream<Arguments>
+      provideShouldReturnResponseStringBasedOnNestedComplexArrayFilters() {
+    return Stream.of(
+        Arguments.of(
+            List.of("complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc2", "Doc4")),
+        Arguments.of(
+            List.of("complexList/any(c:c/nestedComplexArray/any(n:startswith(n/stringVal,'val')))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc2", "Doc4")),
+        Arguments.of(
+            List.of("complexList/any(c:c/nestedComplexArray/any(n:contains(n/stringVal,'match')))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc5", "Doc6")),
+        Arguments.of(
+            List.of(
+                "complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1' or n/stringVal eq 'test1'))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc1", "Doc2", "Doc3", "Doc4")),
+        Arguments.of(
+            List.of(
+                "complexList/any(c:c/nestedComplexArray/any(n:n/stringVal eq 'val1' or n/stringVal eq 'test1') and c/someNumber ge 20)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc2", "Doc3", "Doc4")),
+        Arguments.of(
+            List.of("complexList/any(c:c/nestedComplexArray/any(n:n/numberVal gt 70))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc6")),
+        Arguments.of(
+            List.of(
+                "complexList/any(c:c/nestedComplexArray/any(n:n/numberVal eq 71) and c/nestedComplexArray/any(n:n/numberVal eq 72))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/nestedComplexArray/$count ge 2)"),
+            prepareResponseForQueryWithPlainStringProperties("Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/primitiveStringList/any(n:startswith(n,'item11')))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc6")),
+        Arguments.of(
+            List.of("complexList/any(c:c/primitiveNumberList/any(n:n gt 10))"),
+            prepareResponseForQueryWithPlainStringProperties("Doc6")));
+  }
+
+  @ParameterizedTest
+  @MethodSource({"provideShouldReturnResponseStringBasedOnNestedComplexArrayFilters"})
+  @MongoSetup(
+      mongoDocuments = {
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_1.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_2.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_3.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_4.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_5.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_complex_6.json",
+            collection = "examples"),
+        @MongoDocument(
+            bsonFilePath = "examples/query/example2_only_id.json",
+            collection = "examples")
+      })
+  public void shouldReturnResponseStringBasedOnNestedComplexArrayFilters(
       List<String> filters, String expectedResponse) throws IOException, JSONException {
     // WHEN
     ExtractableResponse<Response> getResponse =
