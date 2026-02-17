489e35e85e8a96ce66933ffc86abef2d990aee61
starnowski <33316705+starnowski@users.noreply.github.com>
2026-02-17 12:35:35 +0100

    Updated tests [skip ci]

--- mongodb-quarkus-changestream/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/changestream/IndexedCollectionChangeStreamListenerTest.java
+++ mongodb-quarkus-changestream/src/test/java/com/github/starnowski/mongo/fun/mongodb/container/changestream/IndexedCollectionChangeStreamListenerTest.java
@@ -7,6 +7,7 @@ import com.mongodb.client.model.changestream.ChangeStreamDocument;
 import com.mongodb.client.model.changestream.OperationType;
 import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
+import org.bson.BsonDocument;
 import org.bson.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -64,11 +65,11 @@ public class IndexedCollectionChangeStreamListenerTest extends AbstractITTest {
         // GIVEN
         String databaseName = "test";
         IndexedCollectionChangeStreamListener listener = new IndexedCollectionChangeStreamListener(mongoClient, databaseName, POSTS_COLLECTION_NAME);
-        BlockingQueue<EventWithCounter> events = new ArrayBlockingQueue<>(2);
+        BlockingQueue<EventWithCounter> events = new ArrayBlockingQueue<>(4);
 
         // WHEN
         listener.startListening((event, counter) -> {
-                if (counter < 3) {
+                if (counter < 2) {
                     events.add(new EventWithCounter(event, counter));
                 }
                 }
@@ -76,16 +77,14 @@ public class IndexedCollectionChangeStreamListenerTest extends AbstractITTest {
         Thread.sleep(2000); // Wait for listener to start
 
         // Perform operations
-        Post post1 = new Post();
-        post1.setText("Test post 1");
-        postDao.save(post1);
 
-        Post post2 = new Post();
-        post2.setText("Test post 2");
-        postDao.save(post2);
+        for (int i = 0; i < 4; i++) {
+            Post post = new Post();
+            post.setText("Test post " + i);
+            postDao.save(post);
+        }
 
         // THEN
-//        events.w
         EventWithCounter result1 = events.poll(20, TimeUnit.SECONDS);
         Assertions.assertNotNull(result1, "Should have received first event");
         Assertions.assertEquals(OperationType.INSERT, result1.event.getOperationType());
@@ -97,7 +96,21 @@ public class IndexedCollectionChangeStreamListenerTest extends AbstractITTest {
         listener.stop();
         Assertions.assertEquals(0, events.size()); // No more events processed
 
+        BsonDocument lastResumeToken = result1.counter == 1L ? result1.event.getResumeToken() : result2.event.getResumeToken();
 
+        // Running listener again to process the last two MongoDB documents
+
+        listener = new IndexedCollectionChangeStreamListener(mongoClient, databaseName, POSTS_COLLECTION_NAME);
+
+        // WHEN
+        listener.startListening((event, counter) -> events.add(new EventWithCounter(event, counter)));
+
+        Thread.sleep(2000); // Wait for listener to start
+        result1 = events.poll(20, TimeUnit.SECONDS);
+        result2 = events.poll(20, TimeUnit.SECONDS);
+        Assertions.assertEquals(Set.of(0L, 1L), Set.of(result1.counter, result2.counter));
+        listener.stop();
+        Assertions.assertEquals(0, events.size()); // No more events processed
     }
 
     private static class EventWithCounter {
