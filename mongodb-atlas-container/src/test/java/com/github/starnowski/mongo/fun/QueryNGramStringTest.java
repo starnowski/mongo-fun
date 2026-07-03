7addd0b1be6c5c57c3c097c49a552eee34e26cac
starnowski <33316705+starnowski@users.noreply.github.com>
2026-07-03 15:15:05 +0200

    Added tests cases

--- mongodb-atlas-container/src/test/java/com/github/starnowski/mongo/fun/QueryNGramStringTest.java
+++ mongodb-atlas-container/src/test/java/com/github/starnowski/mongo/fun/QueryNGramStringTest.java
@@ -11,7 +11,8 @@ import org.bson.Document;
 import org.bson.conversions.Bson;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 public class QueryNGramStringTest extends AbstractItTest {
 
@@ -19,7 +20,8 @@ public class QueryNGramStringTest extends AbstractItTest {
   private static final String DATABASE_NAME = "testdb";
   private static final String COLLECTION_NAME = "filter_phrase_items";
 
-  private static final String NGRAM_INDEX_DEF = """
+  private static final String NGRAM_INDEX_DEF =
+      """
                       {
                         "mappings": {
                           "dynamic": false,
@@ -45,31 +47,77 @@ public class QueryNGramStringTest extends AbstractItTest {
                       }
           """;
 
-  private static final String PHRASE_OPERATOR_TYPE_FIELD_QUERY =
+  private static final String PHRASE_OPERATOR_FIELD1_10_BOOST_FIELD2_1 =
       """
             {
               "$search": {
-                "index": "%s",
-                "phrase": {
-                  "query": "%s",
-                  "path": "type"
+                "index": "%1$s",
+                "compund": {
+                  "should": [
+                    {
+                      "phrase": {
+                        "query": "%2$s",
+                        "path": "field1",
+                        "score": { "boost": 10 }
+                      }
+                    },
+                    {
+                      "phrase": {
+                        "query": "%2$s",
+                        "path": "field2",
+                        "score": { "boost": 1 }
+                      }
+                    }
+                  ]
                 }
               }
             }
             """;
 
+  private static final String PHRASE_OPERATOR_FIELD1 =
+      """
+                {
+                  "$search": {
+                    "index": "%1$s",
+                    "compound": {
+                      "should": [
+                        {
+                          "phrase": {
+                            "query": "%2$s",
+                            "path": "field1"
+                          }
+                        }
+                      ],
+                      "minimumShouldMatch": 1
+                    }
+                  }
+                }
+                """;
+
+  private static java.util.stream.Stream<Arguments>
+      provideShouldReturnExpectedDocumentsWithCorrectOrder() {
+    return java.util.stream.Stream.of(
+        Arguments.of(
+            PHRASE_OPERATOR_FIELD1.formatted(INDEX_NAME, "123"),
+            List.of("QueryNGramStringTest_1", "QueryNGramStringTest_2", "QueryNGramStringTest_3")));
+  }
+
   @ParameterizedTest
-  @ValueSource(strings = {"Groccery", "GROCCERY", "groccery"})
+  @MethodSource("provideShouldReturnExpectedDocumentsWithCorrectOrder")
   @MongoSetup(
       mongoDocuments = {
         @MongoDocument(
             database = DATABASE_NAME,
             collection = COLLECTION_NAME,
-            bsonFilePath = "bson/search/filter_phrase_1.json"),
+            bsonFilePath = "bson/search/QueryNGramStringTest_exact_match.json"),
         @MongoDocument(
             database = DATABASE_NAME,
             collection = COLLECTION_NAME,
-            bsonFilePath = "bson/search/filter_phrase_2.json")
+            bsonFilePath = "bson/search/QueryNGramStringTest_startsWith_match.json"),
+        @MongoDocument(
+            database = DATABASE_NAME,
+            collection = COLLECTION_NAME,
+            bsonFilePath = "bson/search/QueryNGramStringTest_contains_match.json")
       })
   public void shouldReturnExpectedDocumentsWithCorrectOrder(
       String searchQuery, List<String> expectedIds) throws InterruptedException {
@@ -81,7 +129,7 @@ public class QueryNGramStringTest extends AbstractItTest {
 
     List<Bson> pipeline =
         List.of(
-            Document.parse(PHRASE_OPERATOR_TYPE_FIELD_QUERY.formatted(INDEX_NAME, searchQuery)),
+            Document.parse(searchQuery),
             Document.parse(
                 """
             {
@@ -99,6 +147,7 @@ public class QueryNGramStringTest extends AbstractItTest {
         () -> {
           results.clear();
           collection.aggregate(pipeline).into(results);
+          System.out.println("Results ->" + results + "<-");
           // THEN
           Assertions.assertEquals(
               expectedIds,
@@ -115,22 +164,10 @@ public class QueryNGramStringTest extends AbstractItTest {
 
   private void waitForSearchIndexSync(MongoCollection<Document> collection, String indexName)
       throws InterruptedException {
-    waitForSearchIndexSync(collection, indexName, "type");
+    waitForSearchIndexSync(collection, indexName, "field1");
   }
 
   private void ensureSearchIndex(MongoCollection<Document> collection) {
-    ensureSearchIndexReady(
-        INDEX_NAME,
-        """
-          {
-            "mappings": {
-              "dynamic": false,
-              "fields": {
-                "type": { "type": "string" }
-              }
-            }
-          }
-          """,
-        collection);
+    ensureSearchIndexReady(INDEX_NAME, NGRAM_INDEX_DEF, collection);
   }
 }
